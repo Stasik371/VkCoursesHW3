c340779604b858877b99f33db5bd43f29ed937c1
Stasik371 <s.s.kovalev.04@mail.ru>
2022-10-28 01:19:35 +0300

    New dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation ("com.google.code.gson:gson:2.9.1")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
+    implementation("org.projectlombok:lombok:1.18.24")
+    implementation ("org.jetbrains:annotations:13.0")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation ("org.mockito:mockito-core:4.8.1")
+
 }
 
 tasks.getByName<Test>("test") {
